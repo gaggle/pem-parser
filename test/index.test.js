80fe7abb2044db6175e9ca456b9bd52f5deeb994
gaggle <mail@jonlauridsen.com>
2016-02-17 17:27:14 +0100

    trivial

--- test/index.test.js
+++ test/index.test.js
@@ -1,32 +1,32 @@
 "use strict";
 var expect = require("must")
-var index = require("../index")
+var pemParser = require("../")
 
-describe("index", function () {
+describe("pem-parser", function () {
   it("should require an argument", function () {
-    expect(index.bind(this, undefined)).to.throw()
+    expect(pemParser.bind(this, undefined)).to.throw()
   })
 
   it("should respect newlines", function () {
-    var res = index("ham\nspam")
+    var res = pemParser("ham\nspam")
     expect(splitByLines(res)).to.have.length(2)
   })
 
   it("should parse pem key format", function () {
-    var res = index("-----BEGIN RSA PRIVATE KEY----- " +
-                    "aaa " +
-                    "bbb " +
-                    "ccc " +
-                    "-----END RSA PRIVATE KEY-----")
+    var res = pemParser("-----BEGIN RSA PRIVATE KEY----- " +
+                        "aaa " +
+                        "bbb " +
+                        "ccc " +
+                        "-----END RSA PRIVATE KEY-----")
     expect(splitByLines(res)).to.have.length(5)
   })
 
   it("should accept pem format with extra spaces", function () {
-    var res = index(" " +
-                    "-----BEGIN RSA PRIVATE KEY----- " +
-                    "aaa " +
-                    "-----END RSA PRIVATE KEY-----" +
-                    " ")
+    var res = pemParser(" " +
+                        "-----BEGIN RSA PRIVATE KEY----- " +
+                        "aaa " +
+                        "-----END RSA PRIVATE KEY-----" +
+                        " ")
     expect(splitByLines(res)).to.have.length(3)
   })
 })
